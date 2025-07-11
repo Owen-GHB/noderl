9c3bf1d388940fa61d0aca98ad3ab2eefcfcf4ec
Gwilim Owen <gwilim.owen@gmail.com>
2025-07-11 17:48:54 +0100

    hardcode index, removing ejs dependency

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
 const express = require('express');
-const session = require('express-session');
 const bodyParser = require('body-parser');
-const path = require('path');
 const app = express();
 const apiRouter = require('./http-api');
-const creatures = require('./creatures.json');
-const items = require('./items.json');
-const { loadGame, applyGameStateToSession } = require('./savefile.js');
-
-// Set the views folder
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// Configure the session middleware
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true
-}));
 
 // Parse JSON bodies
 app.use(bodyParser.json());
@@ -30,13 +14,6 @@ app.use('/api', apiRouter);
 
 // Serve static files from the /static directory
 app.use('/', express.static(__dirname + '/static'));
-
-app.get('/', (req, res) => {
-  // Render the index.ejs template and pass creaturesData and objectsData as variables
-  res.render('index', { creatures, items });
-});
-
-// Define other routes and logic as needed
 
 // Start the Express server
 app.listen(8000, () => {
